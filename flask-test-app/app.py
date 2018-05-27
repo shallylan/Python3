1f2b4356ca8707cdd657485b8e1e4aec16366c07
shallylan <shallylan>
2018-05-27 15:41:10 +0800

    add app.py

--- flask-test-app/app.py
+++ flask-test-app/app.py
@@ -1,14 +1,18 @@
-from flask import render_template, redirect, url_for, Flask
+from flask import render_template, redirect, url_for, Flask,request,make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('user_index', username='Lou428'))
+    username = request.cookies.get('username')
+    return 'Hello {}'.format(username)
 
 
 @app.route('/user/<username>')
 def user_index(username):
-    return render_template('user.html', username=username)
+    resp = make_response(render_template('user.html', username=username))
+    resp.set_cookie('username', username)
+    return resp
+
 
 
