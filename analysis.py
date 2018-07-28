38f64fbbe67a075c58bb713a7b2813f5103df6da
shallylan <lyn176475051@hotmail.com>
2018-07-28 15:48:44 +0800

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,12 +1,14 @@
-import json
+import json, sys
 import numpy as numpy
 import pandas as pd
 
-def analysis(file，user_id):
-	import pandas as pd
-	data = pd.read_json(file, typ='frame')
-	s = data[data['user_id']== user_id]
+def analysis(file, user_id):
+	data = pd.read_json(file)
+	s = data[data['user_id']== user_id].minutes
 
 	return s.count(), s.sum()
 
-print(analysis('user_study.json', 199071))
+#print(analysis('user_study.json', 199071))
+if __name__ == '__main__':
+    user_id = int(sys.argv[1])
+    print(analysis('user_study.json', user_id))
